5bda5f2285905882cd5f5f5a36c1ee90a4e28fcd
AlexSmith716 <alexsmith716@outlook.com>
2019-05-29 11:21:34 -0400

    comments(redux-saga): Still circling the wagons (learning overload) about replacing thunk with generators versus async/await.

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,20 +1,43 @@
-import { take, put, call, fork, select, all } from 'redux-saga/effects';
+import { call, takeLatest, put, all, race, delay, take, fork  } from 'redux-saga/effects';
+import axios from 'axios';
+
+// 'rootSaga'       responsible for starting other Sagas
+// ----------
+// 'yield select()' returns the result of 'selector(getState())'
+// ----------
+// 'yield all([])'  start all Sagas at once
+// 'yield all([])'  run multiple Effects in parallel and wait for all of them to complete
+// ----------
+// 'yield fork()'   use them in parallel
+// ----------
+// 'fork()'         perform a non-blocking call on `fn`
+// 'fork()'         used to invoke both normal and Generator functions
+// 'fork()'         creates a Task (process running in background) (a redux-saga app can have multiple tasks running in parallel)
+// 'fn: Function'   A Generator function, or normal function which returns a Promise as result
 
 
-/***************************** Subroutines ************************************/
+export function fetchPostsApi(reddit) {
+  return fetch(`https://www.reddit.com/r/${reddit}.json`)
+    .then(response => response.json())
+    .then(json => json.data.children.map(child => child.data))
+}
 
 
+export function* watchFilterableTableDropDownSelected() {
+  console.log('>>>>>>>>> SAGA > sagas > watchFilterableTableDropDownOptionSelected <<<<<<<<<<<<<<')
+  while (true) {
+    const prevReddit = yield select(selectedRedditSelector)
+    yield take(actions.SELECT_REDDIT)
+
+    const newReddit = yield select(selectedRedditSelector)
+    const postsByReddit = yield select(postsByRedditSelector)
+    if (prevReddit !== newReddit && !postsByReddit[newReddit]) yield fork(fetchPosts, newReddit)
+  }
+}
 
 
-/******************************************************************************/
-/******************************* WATCHERS *************************************/
-/******************************************************************************/
-
-
-
-// docs/advanced/RootSaga.md
-export default function* root() {
-  yield fork()
-  yield fork()
-  yield fork()
+export default function* rootSaga() {
+  yield all([
+    fork(watchFilterableTableDropDownSelected)
+  ])
 }
